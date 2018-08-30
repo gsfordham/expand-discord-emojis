dcffeb3c381811e584b83fc2de706c0ff149bdab
gsfordham <rhoblock3@outlook.com>
2018-08-29 20:57:00 -0400

    main file

---
+++ growmoji.js
@@ -0,0 +1,3 @@
+//Set this as a bookmark
+
+javascript: (function() {setInterval(function(){document.querySelectorAll(".emoji").forEach((x)=>{var sz = "75px"; x.style.height = sz; x.style.width = sz;})}, 1000)})();
